d58df8f8fa1ebed932de233e304f97dd681edef0
wincodes <otokinaodafe@gmail.com>
2020-07-05 16:50:00 +0100

    Mini Angular App

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../Models/Todo';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todoLimit:string = '?_limit=10';
+
+  constructor(private http:HttpClient) { }
+
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todoLimit}`);
+  }
+
+  toggleCompleted(todo:Todo):Observable<any>{
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions)
+  }
+
+  deleteTodo(todo: Todo):Observable<any>{
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions )
+  }
+
+  addTodo(todo:Todo):Observable<any>{
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions)
+  }
+}
